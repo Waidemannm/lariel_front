136582cda665a6c2249a6a2c553b5e855dd2497c
Waidemannm <mjrmolinillo@icloud.com>
2026-01-20 21:12:55 -0300

    Refactor code structure for improved readability and maintainability

--- src/routes/Pages/Home/index.tsx
+++ src/routes/Pages/Home/index.tsx
@@ -1,7 +1,9 @@
+import foto from "../../../images/Home/fotoInicial.webp";
+
 export default function Home(){
     return(
         <main className="home-page">
-            
+            <img src={foto} alt="Foto do casal" className="shadow-xl w-full h-full border border-gray-300 rounded-lg object-cover"/>
         </main>
     );
 }
