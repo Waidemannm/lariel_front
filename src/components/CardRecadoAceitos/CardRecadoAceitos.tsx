9b7842f2f7b644cb3253574982afa30aecaf4c0a
Waidemannm <mjrmolinillo@icloud.com>
2026-02-13 19:27:58 -0300

    refactor CardRecadoAceitos component for improved styling and layout

--- src/components/CardRecadoAceitos/CardRecadoAceitos.tsx
+++ src/components/CardRecadoAceitos/CardRecadoAceitos.tsx
@@ -2,10 +2,10 @@ import type { TipoRecadoAceito } from "../../types/tipoRecadoAceito";
 
 export default function CardRecadoAceitos (props: {recadoAceitos: TipoRecadoAceito}) {
     return(
-    <div className="hover:-translate-y-1 transition-all duration-300 relative w-full rounded-2xl border border-black/10 bg-[#FFF8D6] pt-7 p-5 break-words">
+    <div className="hover:-translate-y-1 transition-all duration-300 relative w-full rounded-2xl border-2 border-black/10 bg-[#FFF8D6] pt-7 p-5 break-words">
        
-        <div className="absolute top-3 left-1/2 -translate-x-1/2">
-            <div className="w-4 h-4 rounded-full bg-[var(--color-2)] shadow-sm" />
+        <div className="relative left-1/2 -translate-x-1/2">
+            <div className="w-4 h-4 border-2 border-black/10 rounded-full bg-white shadow-sm" />
         </div>
 
         <h1 className="text-lg font-semibold text-center">{props.recadoAceitos.nomeConvidados}</h1>
