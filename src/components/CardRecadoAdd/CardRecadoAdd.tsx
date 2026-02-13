9ece2e4fd2aea6fee9cd7a444d87c394c02c0693
Waidemannm <mjrmolinillo@icloud.com>
2026-02-13 19:49:26 -0300

    refactor CardRecadoAdd component for improved code readability and structure

--- src/components/CardRecadoAdd/CardRecadoAdd.tsx
+++ src/components/CardRecadoAdd/CardRecadoAdd.tsx
@@ -42,37 +42,37 @@ export default function CardRecadoAdd({open,  onClose, children, }: {open: boole
 
     return(
         <>
-        {loading && (
-        <div className="fixed inset-0 z-[999] flex items-center justify-center bg-black/60">
-            <p className="text-white text-lg font-medium">
-            Salvando recado...
-            </p>
-        </div>
-        )}
+            {loading && (
+            <div className="fixed inset-0 z-[999] flex items-center justify-center bg-black/60">
+                <p className="text-white text-lg font-medium">
+                Salvando recado...
+                </p>
+            </div>
+            )}
 
-        <div className={`fixed inset-0 z-50 flex justify-center items-start md:items-center overflow-y-auto transition-opacity duration-300 ${
-            open ? "opacity-100 bg-black/90" : "opacity-0 pointer-events-none"
-            }`}>
-            
-                <div className={`relative bg-white text-[var(--color-font-black)] rounded-lg shadow p-6 w-[90%] max-w-lg my-10 transition-all duration-300 ${
-                    open ? "scale-100 opacity-100" : "scale-105 opacity-0"
-                    }`}
-                    onClick={(e) => e.stopPropagation()}
-                >
-                    <button className="absolute top-2 right-2 rounded-md text-blue-500 m-1" onClick={onClose}>
-                    Fechar
-                    </button>
+            <div className={`fixed inset-0 z-50 flex justify-center items-start md:items-center overflow-y-auto transition-opacity duration-300 ${
+                open ? "opacity-100 bg-black/90" : "opacity-0 pointer-events-none"
+                }`}>
+                
+                    <div className={`relative bg-white text-[var(--color-font-black)] rounded-lg shadow p-6 w-[90%] max-w-lg my-10 transition-all duration-300 ${
+                        open ? "scale-100 opacity-100" : "scale-105 opacity-0"
+                        }`}
+                        onClick={(e) => e.stopPropagation()}
+                    >
+                        <button className="absolute top-2 right-2 rounded-md text-blue-500 m-1" onClick={onClose}>
+                        Fechar
+                        </button>
 
-                    <div className="flex flex-col gap-3 pl-4 mb-5 mt-10">
-                        <h1 className="text-xl font-bold">Deixe seu recado no mural</h1>
-                        <p className="text-sm text-gray-500">
-                            Seu recado será visível para todos os visitantes do site após o casal aceitar.
-                        </p>
+                        <div className="flex flex-col gap-3 pl-4 mb-5 mt-10">
+                            <h1 className="text-xl font-bold">Deixe seu recado no mural</h1>
+                            <p className="text-sm text-gray-500">
+                                Seu recado será visível para todos os visitantes do site após o casal aceitar.
+                            </p>
+                        </div>
+
+                        <FormRecado register={register} onSubmit={onSubmit} />
                     </div>
-
-                    <FormRecado register={register} onSubmit={onSubmit} />
-                </div>
-        </div>
+            </div>
         </>
     );
 }
