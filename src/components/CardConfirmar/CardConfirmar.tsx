b2ec544cb9679577bc8136a6ebdb9255d2d62ea7
Waidemannm <mjrmolinillo@icloud.com>
2026-04-24 22:14:45 -0300

    fix: arrumando rota confirmar

--- src/components/CardConfirmar/CardConfirmar.tsx
+++ src/components/CardConfirmar/CardConfirmar.tsx
@@ -28,7 +28,7 @@ export default function CardConfirmar({open,  onClose, children, }: {open: boole
             window.location.reload();
         } catch {
             alert("Falha ao buscar convidados. Verifique O ID e o nome do convite.");
-            navigate("/");
+            navigate("/convidados_presenca");
             window.location.reload();
         } finally {
             setLoading(false);
